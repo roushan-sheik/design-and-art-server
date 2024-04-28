582f8a91e42486c195deba9bd62c65e676194f42
roushan-sheik <roushansheik@gmail.com>
2024-04-28 19:35:11 +0600

    api done

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u9zrvau.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 async function run() {
   try {
@@ -101,7 +112,6 @@ run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("This is Arts & Crafts website");
 });
-
 app.listen(port, () => {
   console.log(`This port open in http://localhost:${port}`);
 });
